119f6ac9ed9d8cd6edc57f2e3934f9513e0ee9a1
Gowri Shankar <gowrishankar_m@hotmail.com>
2015-05-07 23:23:48 +0530

    Plot 2, Incomplete - Time vs Global Active Power

--- eda-04/projects/p01/plot2.R
+++ eda-04/projects/p01/plot2.R
@@ -1,2 +1,4 @@
-data <- read.table("household_power_consumption.txt", header=T, sep=';', na.strings="?")
-new_data <- tbl_df(data)
+
+plot_2 <- function(data) {
+    with(data, plot(Time, Global_active_power, type="l", ylab = "Global Active Power (kilowatts)"))
+}
