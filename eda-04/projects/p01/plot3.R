87a0d6a0811500257d19eefd05c5aa382eb13ca1
Gowri Shankar <gowrishankar_m@hotmail.com>
2015-05-07 23:37:28 +0530

    Plot 3, Time vs Sub Meters

--- eda-04/projects/p01/plot3.R
+++ eda-04/projects/p01/plot3.R
@@ -1,2 +1,8 @@
-data <- read.table("household_power_consumption.txt", header=T, sep=';', na.strings="?")
-new_data <- tbl_df(data)
+
+plot_3 <- function(data) {
+    with(data, plot(Time, Sub_metering_1, type="l", ylab = "Energy sub metering"))
+    with(data, lines(Sub_metering_2, type="l", col="red"))
+    with(data, lines(Sub_metering_3, type="l", col="blue"))
+    
+    legend("topright")
+}
