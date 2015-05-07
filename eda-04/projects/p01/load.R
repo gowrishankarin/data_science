3094d9e4044b96c886f6573d9a74612433e5334f
Gowri Shankar <gowrishankar_m@hotmail.com>
2015-05-07 15:20:03 +0530

    Caching HPC data

--- eda-04/projects/p01/load.R
+++ eda-04/projects/p01/load.R
@@ -1,11 +1,42 @@
-load <- function() {
+
+# INPUT <- NONE
+# OUTPUT <- A List Object holding functions to operate on the household power 
+# consumption data to retrieve and store.
+makeHPCData <- function() {
+    data <- NULL
+    setData <- function(new_data) data <<- new_data
+    getData <- function() data
+    list(set = set, setData = setData, getData = getData)
+    
+}
+
+# INPUT <- A function pointer to makeHPCDate method.
+# OUTPUT <- Tidied data of table data frame(tbl_df) for further processing.
+cacheHPCData <- function(x, ...) {
+    data <- x$getData()
+    if(!is.null(data)) {
+        message("Getting cached data")
+        return(data)
+    }
+    
+    new_data <- load()
+    x$setData(new_data)
+    return(new_data)
+}
+
+# INPUT <- File location of HPC data
+# OUTPUT <- Filtered/Tidied data of table data frame(tbl_df) for further 
+# processing.
+load <- function(file = "household_power_consumption.txt") {
+    # Load required libraries
     library(data.table)
     library(dplyr)
     
     # Chain it, Cache It
-
-    data <- read.table("household_power_consumption.txt", header=T, sep=';', 
-        na.strings="?", stringsAsFactors = FALSE)
+    # Read data file and account not available entries as "?" with column 
+    # separator ';'
+    data <- read.table(file, header=T, sep=';', na.strings="?", 
+        stringsAsFactors = FALSE)
     new_data <- tbl_df(data)
     rm(data)
     
@@ -14,6 +45,5 @@ load <- function() {
     new_data <- filter(new_data, Date == as.Date("2007-02-01") | 
         Date == as.Date("2007-02-02"))    
     
-    new_data
-
+    return(new_data)
 }
