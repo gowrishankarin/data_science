1dc932da790545a568a5b3da76517f44298a8efb
Gowri Shankar <gowrishankar_m@hotmail.com>
2015-05-07 23:14:13 +0530

    Plot 1, Global Active Power completed

--- eda-04/projects/p01/plot1.R
+++ eda-04/projects/p01/plot1.R
@@ -1,1 +1,8 @@
 
+
+# Function to plot histogram for Global Active Power
+plot_histogram <- function(data) {
+    with(data, hist(Global_active_power, col="red", 
+                    xlab = "Global Active Power (kilowatts)", 
+                    main = "Global Active Power"))
+}
