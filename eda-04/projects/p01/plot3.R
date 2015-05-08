66b89c608e5e7061f079533f7f515cecf093b2be
Gowri Shankar <gowrishankar_m@hotmail.com>
2015-05-08 22:41:51 +0530

    Plot 3, sub meter data is added

--- eda-04/projects/p01/plot3.R
+++ eda-04/projects/p01/plot3.R
@@ -1,10 +1,13 @@
 
 data <- cacheHPCData(pFunc)
 
-with(data, plot(DateTime, Sub_metering_1, type="l", ylab = "Energy sub metering"))
+# Draw a line graph for DateTime and sub meter 1's utilization
+with(data, plot(DateTime, Sub_metering_1, type="l", xlab = "", ylab = "Energy sub metering"))
 
-    
+# Add lines for sub meter 2 and 3
 with(data, lines(DateTime, Sub_metering_2, type="l", col="red"))
 with(data, lines(DateTime, Sub_metering_3, type="l", col="blue"))
 
-legend("topright")
+# Add a legend @ top right corner.
+legend("topright", lwd=1, lty=1, col = c("black", "blue", "red"), 
+    legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
