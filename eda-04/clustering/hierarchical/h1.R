5b2603503d962c6a438eb600f92165239ba491ba
Gowri Shankar <gowrishankar_m@hotmail.com>
2015-05-09 13:38:37 +0530

    Hierarchical clustering scripts and dedrogram plot

---
+++ eda-04/clustering/hierarchical/h1.R
@@ -0,0 +1,27 @@
+plot1 <- function() {
+    plot(x, y, col = "blue", pch = 19, cex = 2)
+    text(x + 0.05, y + 0.05, labels = as.character(1:12))
+}
+
+
+set.seed(1234)
+par(mar = c(0, 0, 0, 0))
+x <- rnorm(12, mean = rep(1:3, each = 4), sd = 0.2)
+y <- rnorm(12, mean = rep(c(1, 2, 1), each = 4), sd = 0.2)
+
+plot1()
+png(filename = "./pics/h1-plot1.png", width = 480, height = 480, units = "px")
+plot1()
+dev.off()
+
+# Calculate the distance.
+dataFrame <- data.frame(x = x, y = y)
+dist(dataFrame)
+
+hClustering <- hclust(distxy)
+
+plot(hClustering)
+png(filename = "./pics/h1-dendrogram.png", width = 480, height = 480, units = "px")
+plot(hClustering)
+dev.off()
+
