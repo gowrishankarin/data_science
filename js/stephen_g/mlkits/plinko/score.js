a8dbeb83f69c68024ee5bcce98da08a4e58b9a58
Gowri Shankar <shankar.chari@gmail.com>
2018-12-24 19:58:54 +0530

    JS: Plinko Init

---
+++ js/stephen_g/mlkits/plinko/score.js
@@ -0,0 +1,12 @@
+const outputs = [];
+
+function onScoreUpdate(dropPosition, bounciness, size, bucketLabel) {
+  outputs.push([dropPosition, bounciness, size, bucketLabel]);
+
+  console.log(outputs);
+}
+
+function runAnalysis() {
+  // Write code here to analyze stuff
+}
+
