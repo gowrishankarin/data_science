f92fceca96680271fd1731f7302fae132c2afe78
Gowri Shankar <shankar.chari@gmail.com>
2020-07-04 21:19:51 +0530

    COURSERA: Word Embeddings, Vector Spaces and PCA(eigen stuffs)

--- coursera/nlp_spl/c01_classification_vector_spaces/utils.py
+++ coursera/nlp_spl/c01_classification_vector_spaces/utils.py
@@ -150,3 +150,13 @@ def test_lookup(func):
         return 'SUCCESS!!'
     
     return 'Failed Sanity Check'
+
+def get_vectors(embeddings, words):
+    m = len(words)
+    X = np.zeros((1, 300))
+    for word in words:
+        english = word
+        eng_emb = embeddings[english]
+        X = np.row_stack((X, eng_emb))
+    X = X[1:, :]
+    return X
