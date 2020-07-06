224554e9f11c0f74479f51b1610d564019f2c4d4
Gowri Shankar <shankar.chari@gmail.com>
2020-07-06 21:07:59 +0530

    COURSERA: NLP, Vector Manipulation

--- coursera/nlp_spl/c01_classification_vector_spaces/utils.py
+++ coursera/nlp_spl/c01_classification_vector_spaces/utils.py
@@ -5,6 +5,8 @@ from nltk.corpus import stopwords
 from nltk.stem import PorterStemmer
 from nltk.tokenize import TweetTokenizer
 
+import matplotlib.pyplot as plt
+
 def process_tweet(tweet):
     """Process tweet function.
     Input:
@@ -160,3 +162,35 @@ def get_vectors(embeddings, words):
         X = np.row_stack((X, eng_emb))
     X = X[1:, :]
     return X
+
+# Procedure to plot and arrows that represents vectors with pyplot
+def plot_vectors(vectors, colors=['k', 'b', 'r', 'm', 'c'], axes=None, fname='image.svg', ax=None):
+    scale = 1
+    scale_units = 'x'
+    x_dir = []
+    y_dir = []
+    
+    for i, vec in enumerate(vectors):
+        x_dir.append(vec[0][0])
+        y_dir.append(vec[0][1])
+        
+    if ax == None:
+        fig, ax2 = plt.subplots()
+    else:
+        ax2 = ax
+        
+    if axes == None:
+        x_axis = 2 + np.max(np.abs(x_dir))
+        y_axis = 2 + np.max(np.abs(y_dir))
+    else:
+        x_axis = axes[0]
+        y_axis = axes[1]
+        
+    ax2.axis([-x_axis, x_axis, -y_axis, y_axis])
+    
+    for i, vec in enumerate(vectors):
+        ax2.arrow(0, 0, vec[0][0], vec[0][1], head_width=0.05 * x_axis, head_length=0.05 * y_axis, fc=colors[i], ec=colors[i])
+        
+    if(ax == None):
+        plt.show()
+        fig.savefig(fname)
