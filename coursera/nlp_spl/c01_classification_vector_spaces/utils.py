338d9378b55483e84bb81b7b1d961beafdddb104
Gowri Shankar <shankar.chari@gmail.com>
2020-07-12 01:05:36 +0530

    COURSERA: Machine translation

--- coursera/nlp_spl/c01_classification_vector_spaces/utils.py
+++ coursera/nlp_spl/c01_classification_vector_spaces/utils.py
@@ -1,6 +1,7 @@
 import re
 import string
 import numpy as np
+import pandas as pd
 from nltk.corpus import stopwords
 from nltk.stem import PorterStemmer
 from nltk.tokenize import TweetTokenizer
@@ -194,3 +195,37 @@ def plot_vectors(vectors, colors=['k', 'b', 'r', 'm', 'c'], axes=None, fname='im
     if(ax == None):
         plt.show()
         fig.savefig(fname)
+        
+def get_dict(file_name):
+    """
+    This function returns the english to french dictionary given a file where the each column corresponds to a word.
+    Check out the files this function takes in your workspace
+    """
+    my_file = pd.read_csv(file_name, delimiter=' ')
+    etof = {} # the English to French dictionary to be returned
+    for i in range(len(my_file)):
+        # indexing into the rows
+        en = my_file.loc[i][0]
+        fr = my_file.loc[i][0]
+        
+        etof[en] = fr
+        
+    return etof
+
+def cosine_similarity(A, B):
+    """
+    A: a numpy array which corresponds to a word vector
+    B: a numpy array which corre
+    
+    O/P:
+    cos: numerical number representing the cosine similarity between A and B
+    """
+    
+    # You have to set this variable to the true label
+    cos = -10
+    dot = np.dot(A, B)
+    norma = np.linalg.norm(A)
+    normb = np.linalg.norm(B)
+    cos = dot / (norma * normb)
+    
+    return cos
