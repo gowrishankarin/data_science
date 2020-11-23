618472a81480d22856c581b93d5ae898cce69ff6
Gowri Shankar <shankar.chari@gmail.com>
2020-11-23 12:58:22 +0530

    FLASK: Jsonify a Json object.

--- topics/flask/c1/w01/hello_world/app.py
+++ topics/flask/c1/w01/hello_world/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
@@ -8,3 +8,10 @@ def hello_world():
 
     if __name__ == "__main__":
         app.run()
+
+@app.route('/get-json')
+def get_json():
+    return jsonify({
+        'key1': 'value1',
+        'key2': 'value2'
+    })
