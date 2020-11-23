4f34f3724bbd13683db6219927e3e3b1ef5a7af8
Gowri Shankar <shankar.chari@gmail.com>
2020-11-23 20:11:06 +0530

    FLASK: Api methods throught route

--- topics/flask/c1/w01/hello_world/app.py
+++ topics/flask/c1/w01/hello_world/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
@@ -6,12 +6,28 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello World'
 
-    if __name__ == "__main__":
-        app.run()
-
 @app.route('/get-json')
 def get_json():
     return jsonify({
         'key1': 'value1',
         'key2': 'value2'
     })
+
+
+@app.route('/add-two-nums', methods=['POST'])
+def add_two_nums():
+    # Get x and y from posted data
+    data_dict = request.get_json()
+
+    # Add x and y
+    x = data_dict["x"]
+    y = data_dict["y"]
+    z = x + y
+    # Prepare a json
+
+    return jsonify({
+        "z":z
+    }), 200
+
+if __name__ == "__main__":
+        app.run()
