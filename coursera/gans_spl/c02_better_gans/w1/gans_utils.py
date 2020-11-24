b53437bc9bd871bb8159403b2a6cb2db8a73dcda
Gowri Shankar <shankar.chari@gmail.com>
2020-11-25 00:15:59 +0530

    COURSERA: GANs literture and Frechet Distance calculation

---
+++ coursera/gans_spl/c02_better_gans/w1/gans_utils.py
@@ -0,0 +1,12 @@
+import torch
+
+def get_noise(n_samples, z_dim, device='cpu'):
+    '''
+    Function for creating noise vectors: Given the dimensions (n_samples, z_dim)
+    creates a tensor of that shape filled with random numbers from the normal distribution.
+    Parameters:
+        n_samples: the number of samples to generate, a scalar
+        z_dim: the dimension of the noise vector, a scalar
+        device: the device type
+    '''
+    return torch.randn(n_samples, z_dim, device=device)
