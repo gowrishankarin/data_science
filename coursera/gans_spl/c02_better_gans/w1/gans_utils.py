c9041ca08ec7f60a1874c7769fd88b30baf59d11
Gowri Shankar <shankar.chari@gmail.com>
2020-11-27 00:41:16 +0530

    COURSERA: GANs, Perpetual Path Length, Frechet Distance etc

--- coursera/gans_spl/c02_better_gans/w1/gans_utils.py
+++ coursera/gans_spl/c02_better_gans/w1/gans_utils.py
@@ -1,4 +1,6 @@
 import torch
+import matplotlib.pyplot as plt
+from torchvision.utils import make_grid
 
 def get_noise(n_samples, z_dim, device='cpu'):
     '''
@@ -10,3 +12,14 @@ def get_noise(n_samples, z_dim, device='cpu'):
         device: the device type
     '''
     return torch.randn(n_samples, z_dim, device=device)
+
+def show_tensor_images(image_tensor, num_images=16, size=(3, 64, 64), nrow=3):
+    '''
+    Function for visualizing images: Given a tensor of images, number of images,
+    size per image, and images per row, plots and prints the images in an uniform grid.
+    '''
+    image_tensor = (image_tensor + 1) / 2
+    image_unflat = image_tensor.detach().cpu()
+    image_grid = make_grid(image_unflat[:num_images], nrow=nrow)
+    plt.imshow(image_grid.permute(1, 2, 0).squeeze())
+    plt.show()
